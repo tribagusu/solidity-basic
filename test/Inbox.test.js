28f20acb4cd8a1812ecae0c0e72e40ceba17e4fd
tribagus <tribagus.indonesia@gmail.com>
2023-03-14 22:25:16 +0700

    create contract instance

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,6 +2,25 @@ const assert = require("assert");
 const ganache = require("ganache");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+  // get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("should deploys a contract", () => {
+    console.log(inbox);
+  });
+});
 
 // class Car {
 //   park() {
