f8102c447441b9d6e9fee35bd2b1a9e1be6a1a4c
tribagus <tribagus.indonesia@gmail.com>
2023-03-20 15:55:45 +0700

    test deployment

--- deploy.js
+++ deploy.js
@@ -1,1 +1,26 @@
-// deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "brand swift economy rich icon spice boat gift combine bundle ranch bunker",
+  "https://goerli.infura.io/v3/3ed5d4e96e184325a1099bec52e859ab"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there new!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+
+  console.log("contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,9 +1,9 @@
 const assert = require("assert");
 const ganache = require("ganache");
 const Web3 = require("web3");
-const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require("../compile");
 
+const web3 = new Web3(ganache.provider());
 let accounts;
 let inbox;
 
