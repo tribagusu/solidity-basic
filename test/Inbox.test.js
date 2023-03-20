789cb14aa2ec86db4c5e13ceb794db31e05c1f44
tribagus <tribagus.indonesia@gmail.com>
2023-03-20 11:24:13 +0700

    create test for functions

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,43 +6,35 @@ const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
+
 beforeEach(async () => {
   // get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
   // use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
-  it("should deploys a contract", () => {
-    console.log(inbox);
+  // check if contract is already deployed,
+  // and it should have an address
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "bye");
   });
 });
-
-// class Car {
-//   park() {
-//     return "stopped";
-//   }
-
-//   drive() {
-//     return "vroom";
-//   }
-// }
-
-// let car;
-// beforeEach(() => {
-//   car = new Car();
-// });
-
-// describe("Car", () => {
-//   it("can park", () => {
-//     assert.equal(car.park(), "stopped");
-//   });
-
-//   it("can drive", () => {
-//     assert.equal(car.drive(), "vroom");
-//   });
-// });
